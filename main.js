0072fc83c4286fd890cf1f410206604feb2d7f60
biloliddin937523553 <biloliddinlatipov0@gmail.com>
2024-02-28 21:20:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+alert(
+    'Sizga hozir random sonlar chiqishni boshledi'
+)
+let son =Math.floor(Math.random()*70)
+alert(
+    'Random orqali chiqgan son: '+son
+)
+alert(
+    'Biz random sonni 2 ga kopaytirmoqchi va bo`lmoqchimiz!'
+)
+let random = 'Random orqali chiqqan son: '
+let edi = ' edi biz uni 2 ga kopaytirib '
+let son2 = (son +son)
+let son3 = (son/2)
+let son4 = (Math.floor(son/3))
+let son6 = (son4*3)
+let chiqarish = ' sonini chiqardik, va shu sonni 2 ga bolib: '
+let chiqarish2 = ' sonini chiqardik, va shu sonni 3 ga bolib: '
+let son5 = (son-son6)
+let qoldiq = ' ga bolgandagi qoldiq: '
+let chiqarish3 = ' sonini chiqardik'
+alert(
+    random + son +
+    edi + son2 + chiqarish +
+    son3 + chiqarish2 +son4+qoldiq +
+    son5 + chiqarish3
+    
+)
